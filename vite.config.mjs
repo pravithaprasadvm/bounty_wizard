93a5586c5f9a62a74475d49ca82f592bfeb554d3
Pravitha <pravitharasad474@gmail.com>
2025-11-25 23:27:14 +0530

    Fix Vercel base path

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/bounty_wizard/",   
+  base: "/",   // <-- IMPORTANT for Vercel
   plugins: [react()],
 });

2f2a8c6635a75c88791278102cf3aa588de8cdc4
Pravitha V M <pravitharasad474@gmail.com>
2025-11-25 22:09:34 +0530

    Fix GitHub Pages blank screen

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/bounty_wizard/",
+  base: "/bounty_wizard/",   // <-- IMPORTANT
   plugins: [react()],
 });
